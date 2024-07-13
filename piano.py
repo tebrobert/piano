2c8254dc99acca277dbf234b7be4ecdf59c884a9
tebrobert <teb.robert@gmail.com>
2024-07-13 13:49:17 +0400

    up and down

--- piano.py
+++ piano.py
@@ -26,13 +26,14 @@ def game_loop(screen):
         while piano_keys_are_actual:
             for event in pygame.event.get():
                 handle_piano_keypress(event, screen, visible_piano_black_keys, visible_piano_white_keys, visible_piano_keys)
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        first_visible_key_absolute_index = max(5, first_visible_key_absolute_index - 1)
-                        piano_keys_are_actual = False
-                    if event.key == pygame.K_RIGHT:
-                        first_visible_key_absolute_index = min(7*len(piano_octaves)-len(visible_piano_white_keys_keyboard_keys)-6, first_visible_key_absolute_index + 1)
-                        piano_keys_are_actual = False
+                first_visible_key_absolute_index, piano_keys_are_actual = handle_octave_change(event, first_visible_key_absolute_index, piano_keys_are_actual)
+                # if event.type == pygame.KEYDOWN:
+                #     if event.key == pygame.K_LEFT:
+                #         first_visible_key_absolute_index = max(5, first_visible_key_absolute_index - 1)
+                #         piano_keys_are_actual = False
+                #     if event.key == pygame.K_RIGHT:
+                #         first_visible_key_absolute_index = min(7*len(piano_octaves)-len(visible_piano_white_keys_keyboard_keys)-6, first_visible_key_absolute_index + 1)
+                #         piano_keys_are_actual = False
                 if event.type == pygame.QUIT: 
                     running = False
 
@@ -65,6 +66,19 @@ def draw_piano_keys(screen, visible_piano_black_keys, visible_piano_white_keys):
 
     pygame.display.flip()
 
+
+def handle_octave_change(event, first_visible_key_absolute_index, piano_keys_are_actual):
+    if event.type == pygame.KEYDOWN:
+        piano_keys_are_actual = False
+        if event.key == pygame.K_LEFT:
+            first_visible_key_absolute_index = max(5, first_visible_key_absolute_index - 1)
+        if event.key == pygame.K_RIGHT:
+            first_visible_key_absolute_index = min(7*len(piano_octaves)-len(visible_piano_white_keys_keyboard_keys)-6, first_visible_key_absolute_index + 1)
+        if event.key == pygame.K_DOWN:
+            first_visible_key_absolute_index = first_visible_key_absolute_index if first_visible_key_absolute_index - 7 < 5 else first_visible_key_absolute_index - 7
+        if event.key == pygame.K_UP:
+            first_visible_key_absolute_index = first_visible_key_absolute_index if first_visible_key_absolute_index + 7 > 7*len(piano_octaves)-len(visible_piano_white_keys_keyboard_keys)-6 else first_visible_key_absolute_index + 7
+    return first_visible_key_absolute_index, piano_keys_are_actual
 
 def handle_piano_keypress(event, screen, visible_piano_black_keys, visible_piano_white_keys, visible_piano_keys):
     if event.type == pygame.KEYDOWN:
